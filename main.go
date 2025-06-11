2fa9329127f86d62e968b8a21c15dea2b85313a8
Bima Setya Nugraha <setyabima016@gmail.com>
2025-06-11 14:42:17 +0700

    setup awal

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"ewallet-notification/cmd"
+	helpers "ewallet-notification/helpers"
+)
+
+func main() {
+	//load config
+	helpers.SetupConfig()
+
+	//load log
+	helpers.SetupLogger()
+
+	//load database
+	helpers.SetupMySQL()
+
+	//run grpc
+	cmd.ServeGRPC()
+
+	//run http
+	// cmd.ServeHTTP()
+
+}
